54385ffcf6a147cbba26f81a3d4a28a8277e27fc
Nikos <nikoslamprinos13@gmail.com>
2018-02-20 19:19:35 +0200

    telos

--- testUnit/game/Paladin.cpp
+++ testUnit/game/Paladin.cpp
@@ -3,7 +3,7 @@
 /* Constructor */
 Paladin::Paladin(string Name)
 				:Hero(Name,15,12,8,250,250,200){}
-<
+
 /* increase attributes */
 void Paladin::increaseAttributes(){
 	increaseStrength(getLevel()*2); 
